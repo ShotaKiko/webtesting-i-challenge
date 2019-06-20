087ab44603f72c2882cd6f0fe80daf0e362ee552
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-19 22:13:44 -0500

    added unit testing for repair item function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,28 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('the enhancer', () => {
+    
+    describe('the repair function', () => {
+        it('should restore the durability', () => {
+            //arrange
+            const masterSword = {
+                name: 'Master Sword',
+                durability: 20,
+                enhancement: 5
+            }
+
+            //act
+            const repairResult = enhancer.repair(masterSword.durability)
+
+            //assertions
+            expect(repairResult).toBe(100)
+            expect(enhancer.repair()).toBe(100)
+            expect(enhancer.repair('string')).toBe(100)
+        })
+    })
+
+
+
+
+})
